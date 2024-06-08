f7407212aed28bf542a15c56302e815b38ee9c6c
Barun Singh <bxs4749@mavs.uta.edu>
2024-06-08 12:59:58 -0500

    Every functions added

--- Coding Assignment 3/Code3_1002064749.c
+++ Coding Assignment 3/Code3_1002064749.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 
 void fill_numbers (int numbers[5][5]);
 void print_numbers(int numbers[5][5]);
 int pick_number(int *count, int picked_number[75]);
 // void check_duplicate_pick(int picked_number[75]);
+bool number_exist(int numbers[5][5], int pick);
+bool check_row(int numbers[5][5]);
+bool check_column(int numbers[5][5]);
+bool check_diagonal(int numbers[5][5]);
 
 int main(){
     int numbers [5][5];
@@ -14,7 +19,15 @@ int main(){
     srand(time(0));
     fill_numbers(numbers);
     print_numbers(numbers);
-    check_duplicate_pick(picked_number);
+    bool bingo = false;
+    while(!bingo){
+        int pick = pick_number(&count, picked_number);
+        bool exist = number_exist(numbers, pick);
+        if (exist){
+            print_numbers(numbers);
+        }
+        bingo = check_row(numbers) || check_column(numbers) || check_diagonal(numbers);
+    }
 
     return 0;
 }
@@ -46,7 +59,12 @@ void print_numbers(int numbers[5][5]){
         printf("\n");
     for(int i = 0; i < 5; i++){
         for(int j = 0; j < 5; j++){
-            printf("|%4d   ", numbers[i][j]);
+            if(numbers[i][j] == 0){
+                printf("|   X   ");
+            }
+            else{
+                printf("|%4d   ", numbers[i][j]);
+            }
         }
         printf("\n");
         for (int i = 0; i < 5; i++ ){
@@ -54,6 +72,7 @@ void print_numbers(int numbers[5][5]){
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 int pick_number(int *count, int picked_number[75]){
@@ -68,6 +87,81 @@ int pick_number(int *count, int picked_number[75]){
    (*count)++;
     return pick;
 }
+
+bool number_exist(int numbers[5][5], int pick){
+    bool found = false;
+    for (int i = 0; i < 5 ; i++){
+        for (int j = 0; j < 5 ; j++){
+            if ( pick == numbers[i][j]){
+                numbers[i][j] = 0;
+                found = true;
+                goto endloop;
+            }
+        }
+    }
+    endloop:
+    return found;
+}
+
+bool check_row (int numbers[5][5]){
+    bool bingo = false;
+    for(int i = 0 ; i < 5 ; i++){
+        int count = 0;
+        for (int j = 0 ; j < 5; j++){
+            if (numbers[i][j]== 0){
+                count++;
+            }
+        }
+        if (count == 5){
+            bingo = true;
+            printf("You won by row \n");
+            break;
+        }
+    }
+    return bingo;
+}
+
+bool check_column(int numbers[5][5]){
+    bool bingo = false;
+    for(int i = 0; i < 5; i++){
+        int count = 0;
+        for(int j = 0; j < 5 ; j++){
+            if(numbers[j][i] == 0){
+                count++;
+            }
+        }
+        if(count == 5){
+            bingo = true;
+            printf("You won by column");
+            break;
+        }
+    }
+    return bingo;
+}
+
+bool check_diagonal(int numbers[5][5]){
+    bool bingo = false;
+    int mcount = 0;
+    int acount = 0;
+    for(int i = 0; i < 5 ; i++){
+        for (int j = 0; j < 5; j++){
+            if ( i == j && numbers[i][j] == 0){
+                mcount++;
+            }
+            if ( i + j == 4 && numbers [i][j] == 0){
+                acount++;
+            }
+        }
+        if (mcount == 5 || acount == 5){
+            bingo = true;
+            printf("You won by diagonal \n");
+            break;
+        }
+
+    }
+    return bingo;
+}
+
 
 // void check_duplicate_pick(int picked_number[75]){
 //     int check;
